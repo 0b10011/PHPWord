a1632d8a0ac8a6c2008c842e9b12db39bb8fc85a
Brandon Frohs <brandon@19.codes>
2019-08-30 10:43:38 -0400

    Export headings to `<h1>` through `<h6>` (closes #1692)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -30,6 +30,7 @@ This release marked the addition of strict typing and return type declarations (
 - Fixed specifying cell widths, background color, etc on `PhpOffice\PhpWord\Style\Cell` @0b10011 #1669
 - Escape arrays of replacements in `TemplateProcessor` @0b10011 #1669
 - Escape text provided for `<title>` when exporting to HTML @0b10011
+- Export `<h1>` instead of `<p class="Heading1">` for headings @0b10011 #1692
 
 ### Miscellaneous
 -
--- src/PhpWord/Shared/Html.php
+++ src/PhpWord/Shared/Html.php
@@ -22,6 +22,7 @@ use DOMNode;
 use PhpOffice\PhpWord\Element\AbstractContainer;
 use PhpOffice\PhpWord\Element\Row;
 use PhpOffice\PhpWord\Element\Table;
+use PhpOffice\PhpWord\Element\TextRun;
 use PhpOffice\PhpWord\Exception\Exception;
 use PhpOffice\PhpWord\Settings;
 use PhpOffice\PhpWord\Shared\HtmlDpi as Dpi;
@@ -264,20 +265,21 @@ class Html
     /**
      * Parse heading node
      *
-     * @param \PhpOffice\PhpWord\Element\AbstractContainer $element
-     * @param array &$styles
-     * @param string $argument1 Name of heading style
-     * @return \PhpOffice\PhpWord\Element\TextRun
-     *
      * @todo Think of a clever way of defining header styles, now it is only based on the assumption, that
      * Heading1 - Heading6 are already defined somewhere
      */
-    protected static function parseHeading($element, &$styles, $argument1)
+    protected static function parseHeading(AbstractContainer $element, array &$styles, string $headingStyle): TextRun
     {
-        $styles['paragraph'] = $argument1;
-        $newElement = $element->addTextRun($styles['paragraph']);
+        // Create a TextRun to hold styles and text
+        $styles['paragraph'] = $headingStyle;
+        $textRun = new TextRun($styles['paragraph']);
 
-        return $newElement;
+        // Create a title with level corresponding to number in heading style
+        // (Eg, Heading1 = 1)
+        $element->addTitle($textRun, (int) ltrim($headingStyle, 'Heading'));
+
+        // Return TextRun so children are parsed
+        return $textRun;
     }
 
     /**
